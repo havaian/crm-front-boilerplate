2b4871ff522857a98e0675e4cdd1011c26c04cc9
Malik <snapfirecookie@gmail.com>
2022-07-26 15:32:51 +0500

    Leaflet & turf added to the main dir; DrawControl added to Map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -8,6 +8,14 @@
   export default {
     name: "Map",
     mounted() {
+
+      for (let x = 0; x < document.querySelector('.active.exact-active').attributes.length; x++) {
+        if (document.querySelector('.active.exact-active').attributes[x].name == 'modelvalue') {
+          document.querySelector('#page-title').innerHTML = '';
+          document.querySelector('#page-title').append(document.querySelector('.active.exact-active').attributes[x].nodeValue);
+        }
+      }
+
       const map = L.map('map', {
         drawControl: false,
       })
@@ -16,12 +24,40 @@
           maxZoom: 19,
       }).addTo(map);
 
-      for (let x = 0; x < document.querySelector('.active.exact-active').attributes.length; x++) {
-        if (document.querySelector('.active.exact-active').attributes[x].name == 'modelvalue') {
-          document.querySelector('#page-title').innerHTML = '';
-          document.querySelector('#page-title').append(document.querySelector('.active.exact-active').attributes[x].nodeValue);
-        }
-      }
+      let drawControl;
+      let drawnItems = new L.FeatureGroup();
+
+      drawControl = new L.Control.Draw({
+        position: 'topright',
+          draw: {
+          polygon: {
+            allowIntersection: false, // Restricts shapes to simple polygons
+            drawError: {
+                color: 'blue', // Color the shape will turn when intersects
+                message: '<strong>Oh snap!<strong> you can\'t draw that!', // Message that will show when intersect
+            },
+            shapeOptions: {
+                color: 'red',
+            }
+          },
+        circle: false,
+        circlemarker: false,
+        marker: false,
+        polyline: false,
+        rectangle: false,
+      },
+        edit: {
+          featureGroup: drawnItems,
+          remove: true,
+        },
+      });
+
+      map.addControl(drawControl);
+
+      map.on("created", (e) => {
+        
+      })
+
     }
   }
 
