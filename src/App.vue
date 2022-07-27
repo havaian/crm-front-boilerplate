ef8025271b2155d26abc969f6f5c156a7c82a934
Malik <snapfirecookie@gmail.com>
2022-07-27 15:43:19 +0500

    Still playing around with linking

--- src/App.vue
+++ src/App.vue
@@ -23,14 +23,14 @@
           {
             key:'map',
             title: 'Map',
-            icon: './src/assets/map-marker.svg',
+            icon: 'public/assets/map-marker.svg',
             alt: 'Map Icon',
             link: '/',
           },
           {
             key:'profile',
             title: 'Profile',
-            icon: './src/assets/user.svg',
+            icon: 'public/assets/user.svg',
             alt: 'Profile Icon',
             link: 'profile',
           },
