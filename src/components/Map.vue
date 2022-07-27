817757ea95ca48f2bdca3020569476f2311f5f4d
Malik <snapfirecookie@gmail.com>
2022-07-27 15:23:22 +0500

    Changed downloaded plugin system to CDNs

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -25,37 +25,130 @@
       }).addTo(map);
 
       let drawControl;
-      let drawnItems = new L.FeatureGroup();
+      var popup;
+      var layer;
+      var popupContent;
 
-      drawControl = new L.Control.Draw({
+      let drawnItems = new L.FeatureGroup();
+      map.addLayer(drawnItems);
+
+      let options = {
         position: 'topright',
-          draw: {
+        draw: {
           polygon: {
             allowIntersection: false, // Restricts shapes to simple polygons
             drawError: {
-                color: 'blue', // Color the shape will turn when intersects
-                message: '<strong>Oh snap!<strong> you can\'t draw that!', // Message that will show when intersect
+                color: 'red', // Color the shape will turn when intersects
+                message: 'Lines should not intersect', // Message that will show when intersect
             },
             shapeOptions: {
-                color: 'red',
+                color: '#50b8e7',
             }
           },
-        circle: false,
-        circlemarker: false,
-        marker: false,
-        polyline: false,
-        rectangle: false,
-      },
+          circle: false,
+          circlemarker: false,
+          marker: false,
+          polyline: false,
+          rectangle: false,
+        },
         edit: {
           featureGroup: drawnItems,
           remove: true,
         },
-      });
+      }
 
-      map.addControl(drawControl);
+      const reloadDrawControl = () => {
+        if (drawControl === undefined) {
+          drawControl = new L.Control.Draw(options);
+          map.addControl(drawControl);
+        } else {
+          if (!drawControl.getContainer()) {
+            map.removeControl(drawControl);
+            drawControl = new L.Control.Draw(options);
+            map.addControl(drawControl);
+          } else {
 
-      map.on("created", (e) => {
+          }
+        }
+      }
+
+      const loadPopup = (events, layer) => {
         
+        if (events === 'created') {
+          popupContent = `
+            <table id="popup-content">
+              <tr>
+                <td><label for="name"></label>Name</td>
+                <td><input name="name" type="text" required></td>
+              </tr>
+              <tr>
+                <td><label for="area"></label>Area</td>
+                <td><input name="area" type="number" required disabled></td>
+              </tr>
+              <tr>
+                <td><label for="type"></label>Type</td>
+                <td><select name="type" required></select></td>
+              </tr>
+              <tr>
+                <td><label for="description"></label>Description</td>
+                <td><input name="description" type="text" required></td>
+              </tr>
+            </table>
+          `
+          layer.bindPopup(popupContent).openPopup();
+        } else {
+          popupContent = `
+            <table id="popup-content">
+              <tr>
+                <td>Name</td>
+                <td><input></td>
+              </tr>
+              <tr>
+                <td>Area</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Type</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Description</td>
+                <td></td>
+              </tr>
+            </table>
+          `
+        }
+
+      }
+
+      reloadDrawControl();
+
+      map.on("draw:created", (e) => {
+
+        reloadDrawControl();
+
+        // map.removeControl(drawControl);
+        // drawControl = new L.Control.Draw({
+        //     position: 'topright',
+        //     draw: false,
+        //     edit: {
+        //         featureGroup: drawnItems,
+        //         remove: true,
+        //     }
+        // });
+        // map.addControl(drawControl);
+
+        layer = e.layer;
+        drawnItems.addLayer(layer);
+
+        loadPopup('created', layer);
+        
+      })
+
+      map.on("draw:deleted", (e) => {
+
+        reloadDrawControl();
+
       })
 
     }
