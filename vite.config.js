5e2540e8ebb1bae03b53c14e949615a32ba64225
Malik <snapfirecookie@gmail.com>
2022-08-12 17:41:34 +0500

    Proxy settings changed back to prod (localhost)

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       // '/api': 'http://localhost:8080',
       // with options
       '/api': {
-        target: 'https://starlit-swan-172e45.netlify.app',
+        target: 'https://localhost:8080',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
