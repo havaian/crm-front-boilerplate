d5ad9007f91b47c113fc3b3e9a0ba7fdc67aae3d
Malik <snapfirecookie@gmail.com>
2022-08-16 16:00:40 +0500

    Some minor changes & added static.json for Heroku deploy

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'https://construction-map-backend-api.herokuapp.com/',
         changeOrigin: true,
-        secure: false,
+        secure: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
